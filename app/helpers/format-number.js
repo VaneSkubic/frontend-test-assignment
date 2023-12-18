54ea810b6818549c79f62decae80c1a19d3f6202
Vane Skubic <vane.skubic@gmail.com>
2023-12-18 20:54:41 +0100

    fix: Fix table number formatting

---
+++ app/helpers/format-number.js
@@ -0,0 +1,36 @@
+import Ember from 'ember';
+
+export function formatNumber([value, ...rest]) {
+  if (value === null || value === undefined || value === '') {
+    return '/';
+  }
+
+  if (!Number.isInteger(value)) {
+    value = parseFloat(value.toFixed(1));
+    console.log(value);
+  }
+
+  if (value >= 1000000000) {
+    return formatLargeNumber(value, 1e9, 'B');
+  }
+
+  if (value >= 1000000) {
+    return formatLargeNumber(value, 1e6, 'M');
+  }
+
+  if (value >= 1000) {
+    return formatLargeNumber(value, 1000, 'K');
+  }
+
+  return value.toString();
+}
+
+function formatLargeNumber(value, divisor, suffix) {
+  let formattedValue = (value / divisor).toFixed(1);
+  if (formattedValue.endsWith('.0')) {
+    formattedValue = Math.round(formattedValue).toString();
+  }
+  return `${formattedValue}${suffix}`;
+}
+
+export default Ember.Helper.helper(formatNumber);
