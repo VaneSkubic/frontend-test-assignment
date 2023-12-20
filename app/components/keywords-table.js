8eef9dea12effa8ea9f25b19c177699e8247584e
Vane Skubic <vane.skubic@gmail.com>
2023-12-20 13:57:25 +0100

    feat: Sorting by columns

--- app/components/keywords-table.js
+++ app/components/keywords-table.js
@@ -4,20 +4,40 @@ import { tracked } from '@glimmer/tracking';
 
 export default class KeywordsTableComponent extends Component {
     @tracked filterText = '';
+    @tracked sortColumn = 'query';
+    @tracked sortAscending = true;
+
+    @action
+    updateSort(column) {
+        if (this.sortColumn === column) {
+            this.sortAscending = !this.sortAscending;
+        } else {
+            this.sortColumn = column;
+            this.sortAscending = true;
+        }
+    }
 
     @action
     updateFilter(event) {
         this.filterText = event.target.value.toLowerCase();
     }
 
-    @computed('args.keywords', 'filterText')
-    get filteredKeywords() {
-        if (!this.filterText) {
-            return this.args.keywords;
-        }
-
-        return this.args.keywords.filter(keyword =>
+    @computed('args.keywords', 'filterText', 'sortColumn', 'sortAscending')
+    get filteredAndSortedKeywords() {
+        let filteredKeywords = this.args.keywords.filter(keyword =>
             keyword.query.toLowerCase().includes(this.filterText)
         );
+
+        return filteredKeywords.sort((a, b) => {
+            let valueA = a[this.sortColumn];
+            let valueB = b[this.sortColumn];
+
+            if (valueA < valueB) {
+                return this.sortAscending ? -1 : 1;
+            } else if (valueA > valueB) {
+                return this.sortAscending ? 1 : -1;
+            }
+            return 0;
+        });
     }
 }
