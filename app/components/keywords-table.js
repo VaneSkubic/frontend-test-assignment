8f4235cddc77707c3a3dd2877aa2b31d5223b45c
Vane Skubic <vane.skubic@gmail.com>
2023-12-20 14:51:55 +0100

    fix: Fixed table column order

--- app/components/keywords-table.js
+++ app/components/keywords-table.js
@@ -3,41 +3,41 @@ import { action, computed } from '@ember/object';
 import { tracked } from '@glimmer/tracking';
 
 export default class KeywordsTableComponent extends Component {
-    @tracked filterText = '';
-    @tracked sortColumn = 'query';
-    @tracked sortAscending = true;
+  @tracked filterText = '';
+  @tracked sortColumn = 'query';
+  @tracked sortAscending = true;
 
-    @action
-    updateSort(column) {
-        if (this.sortColumn === column) {
-            this.sortAscending = !this.sortAscending;
-        } else {
-            this.sortColumn = column;
-            this.sortAscending = true;
-        }
+  @action
+  updateSort(column) {
+    if (this.sortColumn === column) {
+      this.sortAscending = !this.sortAscending;
+    } else {
+      this.sortColumn = column;
+      this.sortAscending = true;
     }
+  }
 
-    @action
-    updateFilter(event) {
-        this.filterText = event.target.value.toLowerCase();
-    }
+  @action
+  updateFilter(event) {
+    this.filterText = event.target.value.toLowerCase();
+  }
 
-    @computed('args.keywords', 'filterText', 'sortColumn', 'sortAscending')
-    get filteredAndSortedKeywords() {
-        let filteredKeywords = this.args.keywords.filter((keyword) =>
-            keyword.query.toLowerCase().includes(this.filterText),
-        );
+  @computed('args.keywords', 'filterText', 'sortColumn', 'sortAscending')
+  get filteredAndSortedKeywords() {
+    let filteredKeywords = this.args.keywords.filter((keyword) =>
+      keyword.query.toLowerCase().includes(this.filterText),
+    );
 
-        return filteredKeywords.sort((a, b) => {
-            let valueA = a[this.sortColumn];
-            let valueB = b[this.sortColumn];
+    return filteredKeywords.sort((a, b) => {
+      let valueA = a[this.sortColumn];
+      let valueB = b[this.sortColumn];
 
-            if (valueA < valueB) {
-                return this.sortAscending ? -1 : 1;
-            } else if (valueA > valueB) {
-                return this.sortAscending ? 1 : -1;
-            }
-            return 0;
-        });
-    }
+      if (valueA < valueB) {
+        return this.sortAscending ? -1 : 1;
+      } else if (valueA > valueB) {
+        return this.sortAscending ? 1 : -1;
+      }
+      return 0;
+    });
+  }
 }
